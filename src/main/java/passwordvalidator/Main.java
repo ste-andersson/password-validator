6deb97214834a080acf76124d0d3c705ddd6a2b0
Stefan Andersson <stefan.andersson@sveki.se>
2026-01-19 13:35:41 +0100

    Add password length check

--- src/main/java/passwordvalidator/Main.java
+++ src/main/java/passwordvalidator/Main.java
@@ -5,10 +5,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String args[]) {
+        boolean passwordIsValid = false;
         System.out.println("Select a username:");
         Scanner scanner = new Scanner(System.in);
         String username = scanner.next();
         System.out.println("Select a password:");
         String password = scanner.next();
+
+        do {
+            while (!PasswordValidation.isLongEnough(password)) {
+                System.out.println("Password needs to be at least 8 characters. Try again!");
+                password = scanner.next();
+            }
+            if(PasswordValidation.isLongEnough(password)) {
+                passwordIsValid = true;
+            }
+        } while (passwordIsValid = false);
+        System.out.println("Password has been set!");
     }
 }
--- src/main/java/passwordvalidator/PasswordValidation.java
+++ src/main/java/passwordvalidator/PasswordValidation.java
@@ -1,4 +1,13 @@
 package passwordvalidator;
 
 public class PasswordValidation {
+    public static boolean isLongEnough(String password) {
+        boolean isLongEnough;
+        if(password.length() >= 8) {
+            isLongEnough = true;
+        } else{
+            isLongEnough = false;
+        }
+        return isLongEnough;
+    }
 }
